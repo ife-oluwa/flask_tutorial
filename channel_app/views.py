18e3caa55019b2dbee3f94dbcf587fd71fa50557
charles-42 <71760090+charles-42@users.noreply.github.com>
2022-01-16 20:44:54 +0100

    db integrated

--- channel_app/views.py
+++ channel_app/views.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template
-from .channel_dictionary import channel_dictionary
+from .utils import row2dict
 
 app = Flask(__name__)
+
 
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
 
+from .models import Channel
+
 @app.route("/")
 def home():
-    return render_template("index.html", channel_dictionary = channel_dictionary)
+    channel_list = []
+    for channel in Channel.query.with_entities(Channel.title, Channel.description,Channel.image_url).all():
+        channel_list.append(row2dict(channel,column_list=["title","description",'image_url']))
+    return render_template("index.html", channel_list = channel_list)
 
 @app.route("/<channel>")
 def channel(channel):
-    return render_template("channel.html", channel=channel_dictionary[channel])
+    return render_template("channel.html", channel = row2dict(Channel.query.filter_by(title = channel).first()))
+
     
