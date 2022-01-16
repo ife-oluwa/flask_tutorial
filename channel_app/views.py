b432e979d053a4255c07a9038a8c11471d6cead4
charles-42 <71760090+charles-42@users.noreply.github.com>
2022-01-16 22:22:40 +0100

    Post new channel

--- channel_app/views.py
+++ channel_app/views.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for 
+from werkzeug.utils import redirect
 from .utils import row2dict
 
 app = Flask(__name__)
@@ -7,7 +8,7 @@ app = Flask(__name__)
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
 
-from .models import Channel
+from .models import Channel,db
 
 @app.route("/")
 def home():
@@ -20,4 +21,11 @@ def home():
 def channel(channel):
     return render_template("channel.html", channel = row2dict(Channel.query.filter_by(title = channel).first()))
 
-    
+@app.route("/new_channel", methods=["POST",'GET'])
+def new_channel():
+    if request.method =="POST":
+        channel = Channel(**request.form)
+        db.session.add(channel)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("new_channel.html")
